f4337aebff2c91c636b759083eeedc4f3e531b25
Nameka446 <stinger33z@gmail.com>
2014-06-09 11:44:21 +0900

    文字コードを指定

--- main.pl
+++ main.pl
@@ -1,7 +1,10 @@
-=pod
-こっから
+#このソースはUTF8で記述されている
+use utf8;
 
-ここまでコメント
-=cut
-print "Hello world";
+#標準入出力とエラーの文字コードを指定
+binmode STDIN, ':encoding(cp932)';
+binmode STDOUT, ':encoding(cp932)';
+binmode STDERR, ':encoding(cp932';
+
+print "この出力はCP932(SHIFT-JIS拡張)です\n";
 
